6c1d0217066c6181c95eddd0b0f026acc8af7ff7
andrea <andrea@MBP-di-andrea.themis.lan>
2019-02-19 16:40:37 +0100

    fixes timeout op

--- node_modules/stellar-base/test/test-helper.js
+++
@@ -1,7 +0,0 @@
-if (typeof window === 'undefined') {
-  require('babel/register');
-  global.StellarBase = require('../src/index');
-  global.chai = require('chai');
-  global.sinon = require('sinon');
-  global.expect = global.chai.expect;
-}
--- node_modules/stellar-sdk/lib/browser.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = require("./index");
-module.exports.axios = require("axios");
-module.exports.StellarBase = require("stellar-base");
